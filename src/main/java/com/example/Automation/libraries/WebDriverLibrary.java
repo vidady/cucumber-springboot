933962c0c074ca930522218c3e12163bf25fe010
vidady <search4vivek007@gmail.com>
2023-03-12 14:08:00 +0530

    adding argument headless in chrome bean

--- src/main/java/com/example/Automation/libraries/WebDriverLibrary.java
+++ src/main/java/com/example/Automation/libraries/WebDriverLibrary.java
@@ -4,6 +4,7 @@ import com.example.Automation.annotation.LazyConfiguration;
 import com.example.Automation.annotation.ThreadScopeBean;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
@@ -17,7 +18,7 @@ public class WebDriverLibrary {
     @ThreadScopeBean
     @ConditionalOnProperty(name = "browser", havingValue = "chrome")
     public WebDriver getChromeDriver(){
-        return new ChromeDriver();
+        return new ChromeDriver(new ChromeOptions().addArguments("--headless=new"));
     }
 
     @ThreadScopeBean
